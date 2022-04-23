4615e6917b97aed3eb2763199555bf587443737c
MAC17 <mac17@MacBook-Air-de-MAC17.local>
2022-04-23 18:33:08 -0500

    agregar archivos

---
+++ ProjectMVVM/Helper/Poke+Image.swift
@@ -0,0 +1,28 @@
+//
+//  PokeViewModel.swift
+//  ProjectMVVM
+//
+//  Created by MAC17 on 23/04/22.
+//
+
+import Foundation
+import UIKit
+
+class HelperImage {
+    
+    static func setImage(id: String) -> UIImage? {
+        let url = URL(string: setUrlImage(id: id))
+        let data = try? Data(contentsOf: url!)
+        
+        if let imageData = data {
+            let image = UIImage(data: imageData)
+            return image
+        }
+        
+        return nil
+    }
+
+    static func setUrlImage(id: String) -> String {
+        return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(id).png"
+    }
+}
